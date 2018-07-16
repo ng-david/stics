b8a0d05883e43b25fdc267dc529ce667d32e5d56
David Ng <davidn6597@gmail.com>
2018-07-15 19:11:18 -0700

    Add mobile nav

--- js/main.js
+++ js/main.js
@@ -10,4 +10,16 @@ $("#contact-link").click(function() {
     $('html, body').animate({
         scrollTop: $("#contact").offset().top
     }, 400);
+    $('#hamburger-menu').removeClass('show');
 });
+
+$("#mobile-contact-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 600);
+    $('#hamburger-menu').removeClass('show');
+});
+
+$('#hamburger').click(function() {
+  $('#hamburger-menu').toggleClass('show');
+})
